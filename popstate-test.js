bb6546d2fb9112f54e811927316f140016c1787c
scallop722 <scallop722@gmail.com>
2023-08-30 22:17:09 +0900

    iPadのsafari用に画面上でも動作がわかるよう修正

--- popstate-test.js
+++ popstate-test.js
@@ -1,5 +1,6 @@
 history.pushState(null,null,null);
 window.addEventListener("popstate", (e) => {
+  document.getElementById("displayTarget").innerHTML = "ブラウザバックを検知";
   console.log("back");
   alert("back");
 });
