f2ee30cab6d7cf81767b08f8d55dd4e56a65a053
scallop722 <scallop722@gmail.com>
2023-10-02 22:21:06 +0900

    無操作を確認するためのイベント追加

--- interval_test/interval_test.js
+++ interval_test/interval_test.js
@@ -1,6 +1,61 @@
-const timer = setInterval(() => {
+// 30秒
+const popup_millisec = 30 * 1000
+
+const showPopup = () => {
   document.getElementById("displayTarget").innerHTML = "30秒経過";
   console.log("30秒経過");
   alert("30秒経過");
-  clearInterval(timer);
-}, 30 * 1000)
+}
+
+var myTimer = setInterval(showPopup, popup_millisec);
+function clearPopup(){
+  clearInterval(myTimer);
+  myTimer = setInterval(showPopup, popup_millisec);
+}
+
+window.addEventListener('touchstart', function(event) {
+  clearPopup();
+}, false);
+
+window.addEventListener('touchmove', function(event) {
+  clearPopup();
+}, false);
+
+window.addEventListener('touchend', function(event) {
+  clearPopup();
+}, false);
+
+window.addEventListener( "mouseover", function (event) {
+  clearPopup();
+}, false);
+
+window.addEventListener( "mouseout", function (event) {
+  clearPopup();
+}, false);
+
+window.addEventListener( "scroll", function (event) {
+  clearPopup();
+}, false);
+
+window.addEventListener('click', function (event) {
+  clearPopup();
+}, false);
+
+window.addEventListener('input', function (event) {
+  clearPopup();
+}, false);
+
+let input = document.querySelector('input');
+if (input){
+    clearPopup();
+    input.onblur = inputBlur;
+    input.onfocus = inputFocus;
+}
+
+function inputFocus() {
+    clearPopup();
+}
+
+function inputBlur() {
+    clearPopup();
+}
