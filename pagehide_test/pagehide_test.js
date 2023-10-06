5c31138563d0ce3b9c8c758f4b0928d489a3b1a9
scallop722 <scallop722@gmail.com>
2023-10-06 22:27:22 +0900

    画面を開き直さなくても履歴を再描画できるように修正

--- pagehide_test/pagehide_test.js
+++ pagehide_test/pagehide_test.js
@@ -1,13 +1,25 @@
 const storageKey = "pagehideHistory"
 
+// ローカルストレージから読み込み
 let pagehideHistory = JSON.parse(localStorage.getItem(storageKey)) || [];
-        // 配列の各要素をli要素として追加
-const displayTarget = document.getElementById("displayTarget");
-pagehideHistory.forEach(history => {
-  const listItem = document.createElement("li");
-  listItem.textContent = history;
-  displayTarget.appendChild(listItem);
-});
+
+/**
+ * ローカルストレージの内容から履歴を再描画します。
+ */
+const redraw = () => {
+  pagehideHistory = JSON.parse(localStorage.getItem(storageKey)) || [];
+  const displayTarget = document.getElementById("displayTarget");
+  while( displayTarget.firstChild ){
+    displayTarget.removeChild( displayTarget.firstChild );
+  }
+  pagehideHistory.forEach(history => {
+    const listItem = document.createElement("li");
+    listItem.textContent = history;
+    displayTarget.appendChild(listItem);
+  });
+}
+
+redraw();
 
 // 履歴追加
 window.addEventListener("pagehide", () => {
@@ -16,11 +28,11 @@ window.addEventListener("pagehide", () => {
 });
 
 // 履歴削除
-document.getElementById("resetButton").addEventListener("click", () => {
+document.getElementById("dataResetButton").addEventListener("click", () => {
   localStorage.removeItem(storageKey);
   pagehideHistory = [];
-  while( displayTarget.firstChild ){
-    displayTarget.removeChild( displayTarget.firstChild );
-  }
+  redraw();
 });
 
+// 表示リセット
+document.getElementById("displayResetButton").addEventListener("click", redraw);
