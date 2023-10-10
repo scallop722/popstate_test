36b6ac8493c7419d3f39f7da93d1009b349842ab
scallop722 <scallop722@gmail.com>
2023-10-10 21:27:56 +0900

    visibilitychangeのテストページ追加

---
+++ visibilitychange_test/visibilitychange_test.js
@@ -0,0 +1,39 @@
+const storageKey = "visibilitychangeHistory"
+
+// ローカルストレージから読み込み
+let visibilitychangeHistory = JSON.parse(localStorage.getItem(storageKey)) || [];
+
+/**
+ * ローカルストレージの内容から履歴を再描画します。
+ */
+const redraw = () => {
+  visibilitychangeHistory = JSON.parse(localStorage.getItem(storageKey)) || [];
+  const displayTarget = document.getElementById("displayTarget");
+  while( displayTarget.firstChild ){
+    displayTarget.removeChild( displayTarget.firstChild );
+  }
+  visibilitychangeHistory.forEach(history => {
+    const listItem = document.createElement("li");
+    listItem.textContent = history;
+    displayTarget.appendChild(listItem);
+  });
+}
+
+redraw();
+
+// 履歴追加
+document.addEventListener("visibilitychange", () => {
+  visibilitychangeHistory.push(new Date() + ':' + document.visibilityState);
+  localStorage.setItem(storageKey, JSON.stringify(visibilitychangeHistory));
+  redraw();
+});
+
+// 履歴削除
+document.getElementById("dataResetButton").addEventListener("click", () => {
+  localStorage.removeItem(storageKey);
+  visibilitychangeHistory = [];
+  redraw();
+});
+
+// 表示リセット
+document.getElementById("displayResetButton").addEventListener("click", redraw);
